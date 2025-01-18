68609c064de1364208b9517c5ad8bea5b0560e6e
evoly <16191619+Evoly@users.noreply.github.com>
2025-01-18 13:19:51 +0700

    feat: add solution (04-copy-directory)

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,23 @@
+const fsp = require('node:fs/promises');
+const path = require('path');
+
+const dirName = 'files';
+const pathToDir = path.join(__dirname, dirName);
+const copyDir = 'files-copy';
+const pathToCopy = path.join(__dirname, copyDir);
+
+(async () => {
+  try {
+    fsp.mkdir(pathToCopy, { recursive: true });
+
+    const fileList = await fsp.readdir(pathToDir);
+
+    fileList.forEach((file) => {
+      const filepath = path.join(pathToDir, file);
+      const copypath = path.join(pathToCopy, file);
+      fsp.copyFile(filepath, copypath);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})();
